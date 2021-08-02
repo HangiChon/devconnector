5e2b65f08fd15eb84f952b7bfe48000640f442c7
hanpencnd <hanpencnd@gmail.com>
2021-08-02 04:26:41 -0400

    Add Experience/Education components rendered in dashboard

---
+++ client/src/components/dashboard/Education.js
@@ -0,0 +1,46 @@
+import React, { Fragment } from 'react';
+import { connect } from 'react-redux';
+import Moment from 'react-moment';
+import PropTypes from 'prop-types';
+
+const Education = ({ education }) => {
+  const educations = education.map(edu => (
+    <tr key={edu.id}>
+      <td>{edu.company}</td>
+      <td className='hide-sm'>{edu.title}</td>
+      <td>
+        <Moment format='YYYY/MM/DD'>{edu.from}</Moment> -{' '}
+        {edu.to === '' ? (
+          ' Present'
+        ) : (
+          <Moment format='YYYY/MM/DD'>{edu.to}</Moment>
+        )}
+      </td>
+      <td>
+        <button className='btn btn-danger'>Delete</button>
+      </td>
+    </tr>
+  ));
+  return (
+    <Fragment>
+      <h2 className='my-2 list-title'>List of Education</h2>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th className='where'>School</th>
+            <th className='hide-sm what'>Degree</th>
+            <th className='hide-sm when'>Years</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>{educations}</tbody>
+      </table>
+    </Fragment>
+  );
+};
+
+Education.propTypes = {
+  education: PropTypes.array.isRequired
+};
+
+export default Education;
